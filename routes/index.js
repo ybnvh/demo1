3d87bc9cb4ce93bd5cbfa88131ed70e738c58fa4
ybnvh <111330663+ybnvh@users.noreply.github.com>
2023-06-24 08:00:20 +0700

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,85 @@
+var express = require('express');
+const ToyModel = require('../Model/ToyModel');
+var router = express.Router();
+
+router.get('/', async (req, res) => {
+  var toys = await ToyModel.find({});
+
+  // var total = await ToyModel.count();
+  //console.log(Toys);
+  //res.send(toys);
+  res.render('index', { toys : toys })
+})
+
+router.get('/admin',async (req,res) =>{
+  var toys = await ToyModel.find({});
+  res.render('index', {toys: toys})
+})
+
+router.get('/back',async (req,res) =>{
+  var toys = await ToyModel.find({});
+  res.render('product_page', {toys: toys})
+})
+
+router.get('/product_page', async (req, res) => {
+  var toys = await ToyModel.find({});
+  res.render('product_page', { toys: toys });
+})
+
+router.get('/delete/:id', async(req, res) => {
+  await ToyModel.findByIdAndDelete(req.params.id)
+  res.redirect('/');
+})
+
+router.get('/drop', async(req, res) => {
+  await ToyModel.deleteMany({})
+  .then(() => { console.log ('Delete all toys succeed !')});
+  
+  res.redirect('/');
+})
+
+router.get('/add', (req, res) => {
+  res.render('add');
+})
+
+router.post('/add', async (req, res) => {
+  var toy = req.body;
+  await ToyModel.create(toy)
+  .then(() => { console.log ('Add new toy succeed !')});
+  res.redirect('/');
+})
+
+router.get('/edit/:id', async (req, res) => {
+  var toy = await ToyModel.findById(req.params.id);
+  res.render('edit', { toy : toy});
+})
+
+router.post('/edit/:id', async (req, res) => {
+  var id = req.params.id;
+  var updatedData = req.body;
+  await ToyModel.findByIdAndUpdate(id, updatedData)
+    .then(() => { console.log('Edit toy succeed !') });
+  res.redirect('/')
+
+})
+
+
+//sort function
+router.get('/ascending', async (req, res) => {
+  var toys = await ToyModel.find().sort({ price: 1 })
+  res.render('product_page', { toys: toys })
+})
+
+router.get('/descending', async (req, res) => {
+  var toys = await ToyModel.find().sort({ price: -1 })
+  res.render('product_page', { toys: toys })
+})
+
+router.post('/filter',async (req,res) => {
+  var brandName = req.body.brand;
+  var toys = await ToyModel.find().sort({ brand : brandName})
+  res.render('product_page',{toys: toys})
+})
+
+module.exports = router;
+
